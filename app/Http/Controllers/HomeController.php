1a9091686e093539d1b4204beea7e5a6429fcf15
osama mustafa <56209750+osama-mustafa@users.noreply.github.com>
2021-08-15 09:36:41 +0200

    remove comment from controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,16 +6,13 @@ use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
         $this->middleware('auth');
     }
 
+    
     public function index()
     {
         $questions  = DB::table('questions')->get();
