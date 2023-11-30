2a890424a89b880c6f76310bbba08a47745f0f1c
osama-mustafa <semsem6661@gmail.com>
2023-11-30 14:55:32 +0200

    Fix return types in UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,37 +5,38 @@ namespace App\Http\Controllers;
 use App\Http\Requests\UpdateUserRequest;
 use App\Http\Traits\ImageTrait;
 use App\Models\User;
-
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class UserController extends Controller
 {
     use ImageTrait;
 
-    public function index()
+    public function index(): View
     {
         $users = User::latest()->with('questions')->paginate(10);
         return view('admin.users.index', compact('users'));
     }
 
-    public function edit(User $user)
+    public function edit(User $user): View
     {
         return view('admin.users.edit', ['user' => $user]);
     }
 
-    public function update(UpdateUserRequest $request, User $user)
+    public function update(UpdateUserRequest $request, User $user): RedirectResponse
     {
         $user->update($request->validated());
         return back()->with('message', 'User information has been updated successfully');
     }
 
-    public function addToAdmins($id)
+    public function addToAdmins($id): RedirectResponse
     {
         $user = User::findOrFail($id);
         $user->update(['is_admin' => true]);
         return back()->with('message', 'User has been added to admins successfully');
     }
 
-    public function removeFromAdmins($id)
+    public function removeFromAdmins($id): RedirectResponse
     {
         $user = User::findOrFail($id);
         $user->update(['is_admin' => false]);
