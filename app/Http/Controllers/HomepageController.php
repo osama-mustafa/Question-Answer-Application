ac1951d23616d65c26921d51ddb99c90348ae166
osama-mustafa <semsem6661@gmail.com>
2023-11-30 14:25:29 +0200

    Fix return types in HomepageController

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -5,26 +5,28 @@ namespace App\Http\Controllers;
 use App\Models\Question;
 use Illuminate\Http\Request;
 use App\Models\Tag;
+use Illuminate\Contracts\View\View;
 
 class HomepageController extends Controller
 {
 
-    public function homepage()
+    public function homepage(): View
     {
         $questions              = Question::latest()->paginate(10);
-        $most_viewed_questions  = Question::orderBy('views', 'DESC')->take(5)->get(); 
-        return view('homepage', compact('questions', 'most_viewed_questions'));
+        $mostViewedQuestions  = Question::orderBy('views', 'DESC')->take(5)->get();
+        return view('homepage', [
+            'questions' => $questions,
+            'mostViewedQuestions' => $mostViewedQuestions
+        ]);
     }
 
-
-    public function tags()
+    public function tags(): View
     {
         $tags = Tag::paginate(12);
         return view('front.tags.index', compact('tags'));
     }
 
-
-    public function search(Request $request)
+    public function search(Request $request): View
     {
         $request->validate([
             'search' => 'required|string|max:50'
@@ -33,5 +35,4 @@ class HomepageController extends Controller
         $results    = Question::where("title", "LIKE", "%$query%")->paginate(10);
         return view('front.search', compact('results', 'query'));
     }
-
 }
